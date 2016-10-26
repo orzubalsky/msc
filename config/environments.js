0e8911c950baaeadfa7e05c505bf8ea0e6371f76
Or Zubalsky <or.zubalsky@idealist.org>
2016-10-26 15:26:54 -0400

    set dev server host override

--- config/environments.js
+++ config/environments.js
@@ -9,6 +9,7 @@ module.exports = {
   // are served webpack by to fix this issue:
   // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
   development : (config) => ({
+    server_host: '127.0.0.1',
     compiler_public_path : `http://${config.server_host}:${config.server_port}/`
   }),
 
